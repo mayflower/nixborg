62cf804a76c55a365d3e95d003e3126b20f6b124
Robin Gloster <mail@glob.in>
2016-10-08 21:59:54 +0200

    Add logging to git handling

--- nixbot/hydra_jobsets.py
+++ nixbot/hydra_jobsets.py
@@ -2,11 +2,11 @@ import json
 import os.path
 import pygit2
 from pygit2 import RemoteCallbacks, UserPass, Signature
+import logging
 
+log = logging.getLogger(__name__)
 
 class HydraJobsets(object):
-    BRANCH = 'master'
-
     def __init__(self, settings):
         self.repo = "https://github.com/" + settings['nixbot.hydra_jobsets_repo']
         self.repo_path = settings['nixbot.repo_dir']
@@ -17,6 +17,7 @@ class HydraJobsets(object):
 
     def add(self, pr_id):
         pr_name = 'pr-{}'.format(pr_id)
+        log.info('Adding jobset {}'.format(pr_name))
         repo, prs = self._fetch_prs()
         if pr_name not in prs:
             prs.append(pr_name)
@@ -24,6 +25,7 @@ class HydraJobsets(object):
 
     def remove(self, pr_id):
         pr_name = 'pr-{}'.format(pr_id)
+        log.info('Removing jobset {}'.format(pr_name))
         repo, prs = self._fetch_prs()
         if pr_name in prs:
             prs.remove(pr_name)
@@ -32,15 +34,18 @@ class HydraJobsets(object):
     def _fetch_prs(self):
         path = os.path.join(self.repo_path, "hydra-prs.git")
         try:
+            log.info('Cloning {} to {}'.format(self.repo, path))
             repo = pygit2.clone_repository(
                 self.repo, path, bare=False, callbacks=self.creds
             )
         except ValueError:
             repo = pygit2.Repository(path)
-            print('Repo already exists')
 
+        log.info('Fetching origin in {}'.format(path))
         repo.remotes['origin'].fetch(callbacks=self.creds)
+        log.info('Checking out master in {}'.format(path))
         repo.checkout('refs/heads/master')
+        log.info('Resetting hard to origin/master in {}'.format(path))
         repo.reset(repo.lookup_reference('refs/remotes/origin/master').target, pygit2.GIT_RESET_HARD)
 
         with open(os.path.join(path, 'prs.json')) as f:
@@ -49,12 +54,15 @@ class HydraJobsets(object):
     def _save_prs(self, prs, repo, msg):
         path = os.path.join(self.repo_path, "hydra-prs.git")
         with open(os.path.join(path, 'prs.json'), 'w') as f:
+            log.info('Writing to {}'.format(os.path.join(path, 'prs.json')))
             json.dump(prs, f)
 
+        log.info('Staging all changes in {}'.format(path))
         repo.index.add_all()
         author = Signature(self.user, 'bot@nixos.org')
         tree = repo.index.write_tree()
         master = repo.lookup_branch('master')
+        log.info('Commiting changes in {}'.format(path))
         repo.create_commit(
             master.name,
             author, author, msg,
@@ -64,6 +72,7 @@ class HydraJobsets(object):
         repo.state_cleanup()
 
         remote = repo.remotes[0]
+        log.info('Pushing master in {}'.format(path))
         remote.push(['refs/heads/master'], callbacks=self.creds)
 
         repo.state_cleanup()
--- nixbot/pr_merge.py
+++ nixbot/pr_merge.py
@@ -1,6 +1,9 @@
 import os.path
 import pygit2
 from pygit2 import Signature, UserPass, RemoteCallbacks
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def merge_push(pr, base, settings):
@@ -13,27 +16,33 @@ def merge_push(pr, base, settings):
 
     creds = RemoteCallbacks(UserPass(user, token))
 
+    path = os.path.join(REPO_PATH, "nixpkgs.git")
+
     try:
+        log.info('Cloning {} to {}'.format(MAIN_REPO, path))
         repo = pygit2.clone_repository(
-            MAIN_REPO, os.path.join(REPO_PATH, "nixpkgs.git"), bare=False, callbacks=creds
+            MAIN_REPO, path, bare=False, callbacks=creds
         )
     except ValueError:
-        repo = pygit2.Repository(os.path.join(REPO_PATH, "nixpkgs.git"))
-        print('Repo already exists')
+        repo = pygit2.Repository(path)
 
+    log.info('Fetching base repository including PRs')
     repo.remotes.add_fetch('origin', "+refs/pull/*/head:refs/remotes/origin/pr/*")
     repo.remotes['origin'].fetch(callbacks=creds)
 
     try: repo.create_remote('pr', PR_REPO)
     except: pass
 
+    log.info('Checking out and resetting to PR base branch')
     repo.checkout('refs/heads/{}'.format(base))
     repo.reset(repo.lookup_reference('refs/remotes/origin/{}'.format(base)).target, pygit2.GIT_RESET_HARD)
-    base = repo.lookup_branch('base')
+    base = repo.lookup_branch(base)
 
+    log.info('Merging PR {} to base branch'.format(pr))
     origin_pr = repo.lookup_reference('refs/remotes/origin/pr/{}'.format(pr))
     repo.merge(origin_pr.target)
 
+    log.info('Commiting merge')
     author = Signature(settings['nixbot.bot_name'], 'bot@nixos.org')
     tree = repo.index.write_tree()
     repo.create_commit(
@@ -47,6 +56,7 @@ def merge_push(pr, base, settings):
 
     remote = repo.remotes['pr']
     pr_branch = 'pr-{}'.format(pr)
+    log.info('Pushing merge to {} branch at {}'.format(pr_branch, PR_REPO))
     repo.create_branch(pr_branch, repo.head.get_object(), True)
     repo.checkout('refs/heads/{}'.format(pr_branch))
     remote.push(['+refs/heads/{b}:refs/heads/{b}'.format(b=pr_branch)],
