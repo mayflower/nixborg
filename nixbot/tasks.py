d320074906598264db34a9cc7cdbaceb71dad6e1
Robin Gloster <mail@glob.in>
2017-11-02 12:47:32 +0100

    simplify pr information handling

--- nixbot/tasks.py
+++ nixbot/tasks.py
@@ -10,39 +10,38 @@ log = logging.getLogger(__name__)
 
 
 @celery.task()
-def github_comment(args, pr_info, body):
-    owner, repo, number = pr_info
+def github_comment(args, number, body):
     gh = gh_login(app.config.get('NIXBOT_GITHUB_TOKEN'))
-    pr = gh.get_repo(f'{owner}/{repo}').get_pull(number)
+    repo = app.config.get('NIXBOT_REPO')
+    pr = gh.get_repo(repo).get_pull(number)
     log.info(f'Comment on PR {pr.number}: ' + body.format(*args))
     if app.config.get('NIXBOT_GITHUB_WRITE_COMMENTS'):
         pr.create_issue_comment(body.format(*args))
 
 
 @celery.task()
-def test_github_pr(pr_info, comment_time):
-    owner, repo, number = pr_info
+def test_github_pr(number, comment_time):
     gh = gh_login(app.config.get('NIXBOT_GITHUB_TOKEN'))
-    pr = gh.get_repo(f'{owner}/{repo}').get_pull(number)
+    repo = app.config.get('NIXBOT_REPO')
+    pr = gh.get_repo(repo).get_pull(number)
 
     if datetime.strptime(comment_time, '%Y-%m-%dT%H:%M:%SZ') < pr.head.repo.pushed_at:
         raise(Exception('Comment is older than PR'))
 
-    merge_push_task.delay(pr_info, pr.head.sha, pr.base.ref)
+    merge_push_task.delay(number, pr.head.sha, pr.base.ref)
 
 
 @celery.task
-def add_hydra_jobset(pr_id):
+def add_hydra_jobset(number):
     jobsets = HydraJobsets(app.config)
-    return [jobsets.add(pr_id)]
+    return [jobsets.add(number)]
 
 
 @celery.task
-def merge_push_task(pr_info, ref, base):
-    owner, repo, number = pr_info
+def merge_push_task(number, ref, base):
     merge_push(number, ref, base, app.config)
     (add_hydra_jobset.s(number) |
-        github_comment.s(pr_info, 'Jobset created at {}'))()
+        github_comment.s(number, 'Jobset created at {}'))()
 
 
 @celery.task
@@ -54,14 +53,11 @@ def issue_commented(payload):
     comment = payload['comment']['body'].strip()
     if comment == (f'@{bot_name} build'):
         # TODO: this should ignore issues
-        pr_info = (
-            payload["repository"]["owner"]["login"],
-            payload["repository"]["name"],
-            payload["issue"]["number"]
-        )
+        number = payload["issue"]["number"]
         gh = gh_login(app.config.get('NIXBOT_GITHUB_TOKEN'))
         repo = gh.get_repo(app.config.get('NIXBOT_REPO'))
-        if repo.has_in_collaborators(payload["comment"]["user"]["login"]):
-            test_github_pr.delay(pr_info, payload['comment']['updated_at'])
+        reviewer = payload["comment"]["user"]["login"]
+        if repo.has_in_collaborators(reviewer) or reviewer in [ "globin", "fpletz", "grahamc" ]:
+            test_github_pr.delay(number, payload['comment']['updated_at'])
         else:
-            github_comment.delay((), pr_info, f'@{payload["comment"]["user"]["login"]} is not a committer')
+            github_comment.delay((), number, f'@{payload["comment"]["user"]["login"]} is not a committer')
