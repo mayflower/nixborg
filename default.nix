650d913425baad40583e07d22246159f6c6f56b9
Robin Gloster <mail@glob.in>
2017-11-09 09:29:19 +0100

    add module and derivation in this repo

--- default.nix
+++ default.nix
@@ -1,18 +1,52 @@
-with import <nixpkgs> {};
+{ nixborgSrc ? { outPath = ./.; revCount = 1234; rev = "abcdef"; }
+, nixpkgs ? <nixpkgs>
+}:
 
-pkgs.nixborg.server.overrideAttrs (oldAttrs: {
-  src = ./.;
+let
+  pkgs = import nixpkgs{};
+in pkgs.callPackages ({ stdenv, python3, fetchFromGitHub, git }:
+let
+  server = python3.pkgs.buildPythonApplication rec {
+    name = "nixborg-${version}";
+    version = builtins.readFile ./version + "." + toString nixborgSrc.revCount + "." + nixborgSrc.rev;
 
-  shellHook = oldAttrs.shellHook + ''
-    export LC_ALL=en_US.utf8
-    export PATH=${pkgs.git}/bin:$PATH
-    export NIXBORG_SETTINGS=${./development.cfg}
-    export FLASK_DEBUG=1
-    export FLASK_APP=nixborg
-    export APP='python -m flask run --reload'
-    export CELERY='celery -A nixborg.celery worker -E -l INFO'
-    export FLOWER='celery -A nixborg.celery flower'
-    echo 'Run $APP for the flask app, $CELERY for the celery worker'
-    echo 'Run $FLOWER for the celery flower webmonitoring'
-  '';
-})
+    src = nixborgSrc.outPath;
+
+    shellHook = ''
+      export LC_ALL=en_US.utf8
+      export PATH=${git}/bin:$PATH
+      export NIXBORG_SETTINGS=$(pwd)/development.cfg
+      export FLASK_DEBUG=1
+      export FLASK_APP=nixborg
+      export APP='python -m flask run --reload'
+      export CELERY='celery -A nixborg.celery worker -E -l INFO'
+      export FLOWER='celery -A nixborg.celery flower'
+      echo 'Run $APP for the flask app, $CELERY for the celery worker'
+      echo 'Run $FLOWER for the celery flower webmonitoring'
+    '';
+
+    propagatedBuildInputs = with python3.pkgs; [
+      PyGithub flask flask_migrate flask_sqlalchemy celery redis requests
+    ];
+
+    doCheck = false;
+
+    meta = with stdenv.lib; {
+      description = "Github bot for reviewing/testing pull requests with the help of Hydra";
+      maintainers = with maintainers; [ domenkozar fpletz globin ];
+      license = licenses.asl20;
+      homepage = https://github.com/mayflower/nixborg;
+    };
+  };
+  receiver = stdenv.mkDerivation rec {
+    name = "nixborg-receiver-${version}";
+    inherit (server) src version meta;
+
+    buildInputs = [ python3 ];
+    dontBuild = true;
+
+    installPhase = ''
+      install -vD nixborg/receiver.py $out/bin/nixborg-receiver
+    '';
+  };
+in { inherit server receiver; }) {}
